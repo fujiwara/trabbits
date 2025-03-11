ec67d572bc68e23dbffbb031323a356dec72e6b1
fujiwara <fujiwara.shunichiro@gmail.com>
2025-03-11 11:50:00 +0900

    fix logger

--- server_test.go
+++ server_test.go
@@ -12,16 +12,16 @@ import (
 	"github.com/rabbitmq/amqp091-go"
 )
 
-var testServerPort int
+var testProxyPort int
 
-func runTestServer(ctx context.Context) error {
+func runTestProxy(ctx context.Context) error {
 	listener, err := net.Listen("tcp", "localhost:0") // Listen on a ephemeral port
 	if err != nil {
 		slog.Error("Failed to start test server", "error", err)
 		return err
 	}
-	testServerPort = listener.Addr().(*net.TCPAddr).Port
-	go trabbits.BootServer(ctx, listener)
+	testProxyPort = listener.Addr().(*net.TCPAddr).Port
+	go trabbits.BootProxy(ctx, listener)
 	time.Sleep(100 * time.Millisecond) // Wait for the server to start
 	return nil
 }
@@ -33,15 +33,15 @@ var (
 
 func TestMain(m *testing.M) {
 	handler := slog.Default().Handler()
-	logger = slog.New(handler).With("client", "test")
+	logger = slog.New(handler).With("test", true)
 
-	runTestServer(serverCtx)
+	runTestProxy(serverCtx)
 	defer cancel()
 	m.Run()
 }
 
-func TestServerConnect(t *testing.T) {
-	conn, err := amqp091.Dial(fmt.Sprintf("amqp://guest:guest@127.0.0.1:%d/", testServerPort))
+func TestProxyConnect(t *testing.T) {
+	conn, err := amqp091.Dial(fmt.Sprintf("amqp://guest:guest@127.0.0.1:%d/", testProxyPort))
 	if err != nil {
 		t.Fatal(err)
 	}
