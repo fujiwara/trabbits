c98021eaa511adb04c3fc13158f89e8e69cceee5
fujiwara <fujiwara.shunichiro@gmail.com>
2025-03-19 22:47:13 +0900

    add license

--- server.go
+++ server.go
@@ -3,6 +3,9 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
+// MIT License
+// Copyright (c) 2025 FUJIWARA Shunichiro
+
 package trabbits
 
 import (
--- version.go
+++ version.go
@@ -1,3 +1,6 @@
+// MIT License
+// Copyright (c) 2025 FUJIWARA Shunichiro
+
 package trabbits
 
 var Version = "0.0.1"
